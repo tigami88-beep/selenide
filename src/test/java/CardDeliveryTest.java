d0b1af4de2d807044bc9ad317b97b32aae6b9241
tigami88-beep <tigami88@gmail.com>
2026-09-10 15:02:37 +0300

    first commit

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,6 +1,4 @@
-
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,7 +8,7 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Selenide.\$;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTest {
@@ -29,29 +27,29 @@ public class CardDeliveryTest {
     void shouldSuccessfullySubmitCardDeliveryForm() {
         // 1. Город (случайный из JSON)
         String randomCity = CitiesLoader.getRandomCity();
-        \$("#city").setValue(randomCity);
+        $("#city").setValue(randomCity);
 
         // 2. Дата (через 3 дня)
         String validDate = getValidDate();
-        \$("#date").setValue(validDate);
+        $("#date").setValue(validDate);
 
         // 3. ФИО
-        \$("#fullName").setValue("Иван Петров");
+        $("#fullName").setValue("Иван Петров");
 
         // 4. Телефон
-        \$("#phone").setValue("+79000000000");
+        $("#phone").setValue("+79000000000");
 
         // 5. Согласие (чекбокс)
-        \$("#agreement").click();
+        $("#agreement").click();
 
         // 6. Кнопка «Забронировать»
-        \$("#bookButton").click();
+        $("#bookButton").click();
 
         // 7. Проверка загрузки (ждем появления элемента)
-        \$("#loading").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $("#loading").shouldBe(Condition.visible, Duration.ofSeconds(15));
 
         // 8. Проверка успеха
-        \$("#success-modal").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $("#success-modal").shouldBe(Condition.visible, Duration.ofSeconds(15));
     }
 
     private String getValidDate() {
